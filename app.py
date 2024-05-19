1a3cd3a1f189dc2b33bbcb2707f977790980f970
diamondhanddevOps <shittuabiola35@gmail.com>
2024-05-18 23:43:35 -0400

    commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'New York, USA',
+    'salary': '$150,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Maryland, USA',
+    'salary': '$150,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
+
+@app.route("/")
+def hello_abi():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='abi')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
